2d6e47206d66820045849590412ab29bf01e07dc
Francis Kofi Crentsil <franciskoficrentsil@gmail.com>
2025-12-05 02:26:28 +0000

    initial commit: my first asignment

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = 'Francis Kofi Crentsil';
+const age = 20;
+const fav = 'Python';
+
+console.log(' name: ', name);
+console.log('age: ', age);
+console.log('favorite program languange: ', fav);
